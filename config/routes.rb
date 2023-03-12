5476cd6122f3f0d86cdab51a6070901a58e0af22
iayaz1 <iamlordayaz@gmail.com>
2023-03-12 21:21:28 +0530

    Created an session and chatroom controller abd added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'chatroom#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'login' , to: 'sessions#new'
 end
